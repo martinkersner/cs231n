13374a8aa131abcac0e07f37538368c14aedc9cd
Martin Keršner <m.kersner@gmail.com>
2015-10-26 12:30:37 +0900

    naive implementation of soft max function

---
+++ softmax.py
@@ -0,0 +1,71 @@
+import numpy as np
+from random import shuffle
+
+def softmax_loss_naive(W, X, y, reg):
+  """
+  Softmax loss function, naive implementation (with loops)
+  Inputs:
+  - W: C x D array of weights
+  - X: D x N array of data. Data are D-dimensional columns
+  - y: 1-dimensional array of length N with labels 0...K-1, for K classes
+  - reg: (float) regularization strength
+  Returns:
+  a tuple of:
+  - loss as single float
+  - gradient with respect to weights W, an array of same size as W
+  """
+  # Initialize the loss and gradient to zero.
+  loss = 0.0
+  dW = np.zeros_like(W)
+
+  #############################################################################
+  # Compute the softmax loss and its gradient using explicit loops.           #
+  # Store the loss in loss and the gradient in dW. If you are not careful     #
+  # here, it is easy to run into numeric instability. Don't forget the        #
+  # regularization!                                                           #
+  #############################################################################
+  num_classes = W.shape[0]
+  num_train = X.shape[1]
+
+  for i in xrange(num_train):
+    scores = W.dot(X[:, i])
+    actual_correct_score = scores[y[i]]
+
+    sum_exp_scores = np.sum(np.exp(scores))
+    f_yi = -np.exp(actual_correct_score) / sum_exp_scores
+
+    e_f_j = 0
+
+    for j in xrange(num_classes):
+      e_f_j += np.exp(np.exp(scores[j]) / sum_exp_scores)
+
+    loss += f_yi + np.log(e_f_j)
+
+  loss /= num_train
+  loss += 0.5 * reg * np.sum(W * W)
+
+  return loss, dW
+
+
+def softmax_loss_vectorized(W, X, y, reg):
+  """
+  Softmax loss function, vectorized version.
+
+  Inputs and outputs are the same as softmax_loss_naive.
+  """
+  # Initialize the loss and gradient to zero.
+  loss = 0.0
+  dW = np.zeros_like(W)
+
+  #############################################################################
+  # TODO: Compute the softmax loss and its gradient using no explicit loops.  #
+  # Store the loss in loss and the gradient in dW. If you are not careful     #
+  # here, it is easy to run into numeric instability. Don't forget the        #
+  # regularization!                                                           #
+  #############################################################################
+  pass
+  #############################################################################
+  #                          END OF YOUR CODE                                 #
+  #############################################################################
+
+  return loss, dW

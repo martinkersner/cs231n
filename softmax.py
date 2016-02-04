rewritten naive loss, added naive gradient computation and fixed erorr in vectorized gradient computation

softmax.py
@@ -29,6 +29,7 @@ def softmax_loss_naive(W, X, y, reg):
   # here, it is easy to run into numeric instability. Don't forget the        #
   # regularization!                                                           #
   #############################################################################
+  num_dims = W.shape[0]
   num_classes = W.shape[1]
   num_train = X.shape[0]
 
@@ -36,10 +37,22 @@ def softmax_loss_naive(W, X, y, reg):
     scores = X[i, :].dot(W)
     exp_scores = np.exp(scores)
 
-    loss += -np.log(exp_scores[y[i]] / np.sum(exp_scores))
+    prob_scores = exp_scores/np.sum(exp_scores)
+
+    for d in xrange(num_dims):
+        for k in xrange(num_classes):
+            if k == y[i]:
+                dW[d, k] += X.T[d, i] * (prob_scores[k]-1)
+            else:
+                dW[d, k] += X.T[d, i] * prob_scores[k]
+    
+    loss += -np.log(prob_scores[y[i]])
 
   loss /= num_train
-  loss += 0.5 * reg * np.sum(W * W)
+  loss += 0.5 * reg * np.sum(W**2)
+  
+  dW /= num_train
+  dW += reg * W
 
   return loss, dW
 
@@ -55,7 +68,7 @@ def softmax_loss_vectorized(W, X, y, reg):
   dW = np.zeros_like(W)
 
   #############################################################################
-  # TODO: Compute the softmax loss and its gradient using no explicit loops.  #
+  # Compute the softmax loss and its gradient using no explicit loops.        #
   # Store the loss in loss and the gradient in dW. If you are not careful     #
   # here, it is easy to run into numeric instability. Don't forget the        #
   # regularization!                                                           #
@@ -70,9 +83,11 @@ def softmax_loss_vectorized(W, X, y, reg):
   loss /= num_train
   loss += 0.5 * reg * np.sum(W**2)
 
-  # grads 
-  dscores = prob_scores 
+  # grads
+  dscores = prob_scores
+  dscores[range(num_train), y] -= 1
   dW = np.dot(X.T, dscores)
-  dW += 0.5*reg*W
+  dW /= num_train
+  dW += reg * W
 
   return loss, dW
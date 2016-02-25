7d781b8b52af98cd55bf5f0a83345f2a191430d6
Martin Keršner <m.kersner@gmail.com>
2016-02-25 18:35:01 +0900

    convert to inverted dropout

--- assignment2/layers.py
+++ assignment2/layers.py
@@ -307,13 +307,13 @@ def dropout_forward(x, dropout_param):
     # Implement the training phase forward pass for inverted dropout.         #
     # Store the dropout mask in the mask variable.                            #
     ###########################################################################
-    mask = np.random.rand(*x.shape) < p
+    mask = (np.random.rand(*x.shape) < p) / p
     out = x * mask
   elif mode == 'test':
     ###########################################################################
     # Implement the test phase forward pass for inverted dropout.             #
     ###########################################################################
-    out = x * p
+    out = x
 
   cache = (dropout_param, mask)
   out = out.astype(x.dtype, copy=False)
